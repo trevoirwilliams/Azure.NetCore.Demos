ee3434bbb9a667032b9e0993ace2b0ae30052429
Trevoir Williams <GitHub Email>
2023-04-03 09:17:58 -0500

    Added Service Bus Demo Sender and Reciever

---
+++ Console.ServiceBusReceiver.Demo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "";
+
+const string queueName = "";
+const string topicName = "";
+const string sub1Name = "";
+ServiceBusClient client;
+ServiceBusProcessor processor = default!;
+
+async Task MessageHandler(ProcessMessageEventArgs processMessageEventArgs)
+{
+    string body = processMessageEventArgs.Message.Body.ToString();
+    Console.WriteLine($"{body} - Subscription: {sub1Name}");
+    //Console.WriteLine($"{body}");
+    await processMessageEventArgs.CompleteMessageAsync(processMessageEventArgs.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs processMessageEventArgs)
+{
+    Console.WriteLine(processMessageEventArgs.Exception.ToString());
+    return Task.CompletedTask;
+}
+
+client = new ServiceBusClient(serviceBusConnectionString);
+//processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+processor = client.CreateProcessor(topicName, sub1Name, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Press any key to end the processing");
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ Console.ServiceBusSender.Demo/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "";
+
+//const string queueName = "";
+const string topicName = "";
+const int maxNumberofMessages = 3;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+
+client = new ServiceBusClient(serviceBusConnectionString);
+sender = client.CreateSender(topicName);
+//sender = client.CreateSender(queueName);
+
+using ServiceBusMessageBatch batch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= maxNumberofMessages; i++)
+{
+    if (!batch.TryAddMessage(new ServiceBusMessage($"This a message - {i}")))
+    {
+        Console.WriteLine($"Message - {i} was not added to the batch");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(batch);
+    Console.WriteLine("Messages Sent");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Exception: {ex.Message}");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
